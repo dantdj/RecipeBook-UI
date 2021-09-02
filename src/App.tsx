3dbd31dc3d56f562d4fa5a38515e065a573a2ee9
Daniel Temple <dantdj@gmail.com>
2021-09-02 18:52:24 +0100

    Initial Google login

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { GoogleLogin } from 'react-google-login';
 
 function App() {
+  const responseGoogle = (response: any) => {
+    console.log(response);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +23,14 @@ function App() {
         >
           Learn React
         </a>
+
+        <GoogleLogin 
+        clientId="425880518319-3fsfb0c616uvkrp52mpuh7f7mj0smuhb.apps.googleusercontent.com"
+        buttonText="Log in with Google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
       </header>
     </div>
   );
